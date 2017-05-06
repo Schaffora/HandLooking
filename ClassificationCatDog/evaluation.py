911121d71ad76ed995ed0b022537447733bafac9
llmmg <lancelotlancelot94@gmail.com>
2017-05-06 15:18:15 +0200

    evaluation.py args + screenshot of model10k

--- ClassificationCatDog/evaluation.py
+++ ClassificationCatDog/evaluation.py
@@ -3,39 +3,38 @@ import keras
 import numpy as np
 import cv2
 import os
+import sys
 
 
+# Eval images find in /eval/ directories.
 def multipleEval():
+    # store image in here
     imLoad = []
 
-    expCat = 0
-    expDog = 0
-    directories = ["/eval/cat/", "/eval/dog/"]
-    # read files in folders cat dog
+    # numbers of files found in directories
+    expCat = 0  # expected cat
+    expDog = 0  # expected dog
 
-    for f in os.listdir(os.getcwd() + directories[0]):
-        # imLoad.append([cv2.imread(f), f])
-        fullName = os.getcwd() + directories[0] + f
+    directories = ["eval/cat/", "eval/dog/"]
+
+    # read files in folders cat dog
+    for f in os.listdir(directories[0]):
+        # /eval/cat/ + fileName
+        fullName = directories[0] + f
         imLoad.append(fullName)
         expCat += 1
 
-    for f in os.listdir(os.getcwd() + directories[1]):
-        fullName = os.getcwd() + directories[1] + f
-        # imLoad.append([cv2.imread(f), f])
+    for f in os.listdir(directories[1]):
+        fullName = directories[1] + f
         imLoad.append(fullName)
         expDog += 1
 
     numCat = 0
-    maybeCat = 0
     numDog = 0
-    maybeDog = 0
 
     # for img, name in imLoad:
     for name in imLoad:
         loadedImage = cv2.imread(name)
-        # cv2.imshow(name, loadedImage)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
 
         if (loadedImage is not None):
             img = cv2.resize(loadedImage, (150, 150))
@@ -48,34 +47,27 @@ def multipleEval():
         gray_image = img.reshape((1,) + img.shape)
         # print('prediction of [1, 1]: ', loaded_model.predict_classes(img, verbose=verbose))
 
-
-        # seem to be: 1 cats, 0 dogs
         y_proba = loaded_model.predict(gray_image)
-        print("proba:",y_proba)
         y_classes = loaded_model.predict_classes(gray_image)
-        print("classe:",y_classes)
+
+        print("proba:", y_proba)
+        print("classe:", y_classes)
+        # print(loaded_model.metrics_names)
 
         if y_classes == 0:
-            # if y_proba[0][0] < 0.5:
-            #     maybeDog += 1
-            # else:
             numCat += 1
         elif y_classes == 1:
-            # if y_proba[0][0] < 0.5:
-            #     maybeCat += 1
-            # else:
             numDog += 1
-            # y_classes = keras.np_utils.probas_to_classes(y_proba)
 
     print("cat expected:", expCat)
     print("cat found:", numCat)
     print("dog expected:", expDog)
     print("dog found:", numDog)
-    print("maybe dog:", maybeDog)
-    print("maybe cat:", maybeCat)
 
 
+# eval the given file
 def one(file):
+    # load and display file
     loadedImage = cv2.imread(file)
     cv2.imshow("img", loadedImage)
     cv2.waitKey(0)
@@ -96,38 +88,35 @@ def one(file):
     # seem to be: 1 cats, 0 dogs
     y_proba = loaded_model.predict(gray_image)
     print(y_proba)
-    y_classes = loaded_model.predict_classes(gray_image)
+    y_classes = loaded_model.predict_classes(gray_image, verbose=0)
     print(y_classes)
 
-    if y_classes == 0:
+    if y_classes == 1:
         print("It's a cat!")
-    elif y_classes == 1:
+    elif y_classes == 0:
         print("It's a dog!")
 
 
-if __name__ == "__main__":
-    Choices = ["modelWith10k/", ""]
-    modelChoice = Choices[0] #0-1 to change model
-
+def loadModel(modelName):
     # load json and create model
     # json_file = open('model.json', 'r')
-    json_file = open(modelChoice + 'model.json', 'r')
+    json_file = open(modelName + '/model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
 
     # load weights into new model
     # loaded_model.load_weights("model.h5")
-    loaded_model.load_weights(modelChoice + "model.h5")
+    loaded_model.load_weights(modelName + "/model.h5")
     print("Loaded model from disk")
 
     img_width, img_height = 150, 150
     loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-    verbose = 0
 
-    #eval the files in /eval/ directories
-    multipleEval()
+    return loaded_model
 
+
+def testSomeFiles():
     list = ["cat.12497.jpg", "cat.12498.jpg", "cat.12499.jpg",
             "dog.12497.jpg", "dog.12498.jpg", "dog.12499.jpg"]
 
@@ -137,5 +126,29 @@ if __name__ == "__main__":
     # one(os.getcwd()+"\\"+list[3]) # marche pas avec le model1
     # one(os.getcwd()+"\\"+list[4]) # faux
     # one(os.getcwd()+"\\"+list[5])
-    # one(os.getcwd()+"\\"+"chat.jpg")
+    # one("chat.jpg")
+    # one("dog1.jpg")
 
+
+if __name__ == "__main__":
+    # Get args
+    print(sys.argv)
+    modelName = sys.argv[1]
+    if len(sys.argv) < 3:
+        imageFile = None
+    else:
+        imageFile = sys.argv[2]
+
+    # loaded_model = 0
+
+    # Load images
+    if (modelName is not None):
+        loaded_model = loadModel(modelName)
+    else:
+        print("No model given. Please give the absolute path to your model")
+
+    if (imageFile is not None):
+        one(imageFile)
+    else:
+        # eval the files in /eval/ directories
+        multipleEval()
